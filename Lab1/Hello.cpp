e5b7dcd8a383fcfb97d0563de11f846833f1c5bc
aimejia <aimejias05@gmail.com>
2026-08-26 17:10:58 -0700

    Initial Commit

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,12 @@
+/***
+ * {Hello World Demonstration}
+ ***/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+} 
